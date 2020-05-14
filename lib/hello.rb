4ae0ce4b94634cea9042154b3095b9af58bc3d81
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-14 01:32:00 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,13 @@
 def hello_t(array)
-  i = 0
-  
-  while i < array.length
-    yield array[i]
-    i = i + 1
+  if block_given?
+    i = 0
+    
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+  else
+    puts "no block_given"
   end
 end
 
@@ -13,4 +17,5 @@ hello_t(['tom', "tim", "jim"]) do |name|
   if name.upcase.start_with?("T")
     puts "Hi, #{name}"
   end
+  
 end
